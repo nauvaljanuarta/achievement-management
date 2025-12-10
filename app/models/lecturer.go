f48009d06c849ff199d4bb37f070123e0c1f1974
ndajatoh01 <nauvalaryanto2005@gmail.com>
2025-12-10 19:01:12 +0700

    add lecturer models and fix major (v5.5.6)

--- app/models/lecturer.go
+++ app/models/lecturer.go
@@ -6,9 +6,30 @@ import (
 )
 
 type Lecturer struct {
+	ID         uuid.UUID `json:"id" db:"id"`
+	UserID     uuid.UUID `json:"userId" db:"user_id"`
+	LecturerID string    `json:"lecturerId" db:"lecturer_id"`
+	Department string    `json:"department" db:"department"`
+	CreatedAt  time.Time `json:"createdAt" db:"created_at"`
+}
+
+type CreateLecturerProfileRequest struct {
+	LecturerID string `json:"lecturerId" binding:"required"`
+	Department string `json:"department" binding:"required"`
+}
+
+type UpdateLecturerRequest struct {
+	LecturerID *string `json:"lecturerId,omitempty"`
+	Department *string `json:"department,omitempty"`
+}
+
+type LecturerResponse struct {
 	ID         uuid.UUID `json:"id"`
-	UserID     uuid.UUID `json:"user_id"`
-	LecturerID string    `json:"lecturer_id"`
+	UserID     uuid.UUID `json:"userId"`
+	FullName   string    `json:"fullName"`
+	Username   string    `json:"username"`
+	Email      string    `json:"email"`
+	LecturerID string    `json:"lecturerId"`
 	Department string    `json:"department"`
-	CreatedAt  time.Time `json:"created_at"`
+	CreatedAt  time.Time `json:"createdAt"`
 }
