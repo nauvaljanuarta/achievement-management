757c1fd85e2131a7f5c97648fb22fac7e837719a
ndajatoh01 <nauvalaryanto2005@gmail.com>
2025-12-10 19:21:54 +0700

    add role permission to role models minor (v5.6.9)

--- app/models/role.go
+++ app/models/role.go
@@ -8,8 +8,12 @@ import (
 
 type Role struct {
 	ID          uuid.UUID `json:"id" db:"id"`
-	Name        string    `json:"name" db:"name"` // Admin, Mahasiswa, dosen Wali
+	Name        string    `json:"name" db:"name"` 
 	Description string    `json:"description" db:"description"`
 	CreatedAt   time.Time `json:"createdAt" db:"created_at"`
 }
 
+type RolePermission struct {
+	RoleID       uuid.UUID `json:"roleId" db:"role_id"`           
+	PermissionID uuid.UUID `json:"permissionId" db:"permission_id"` 
+}
