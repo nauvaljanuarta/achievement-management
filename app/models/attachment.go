0020a841c28ead15add0b5f4f042e6d4b5d451f5
ndajatoh01 <nauvalaryanto2005@gmail.com>
2025-12-14 08:41:35 +0700

    adjusting attachment variable minor (v9.10.17)

--- app/models/attachment.go
+++ app/models/attachment.go
@@ -1,12 +1,15 @@
-package models
+	package models
 
-import (
-	"time"
-)
+	import (
+		"time"
+		"github.com/google/uuid"
+	)
 
-type Attachment struct {
-	FileName   string    `bson:"fileName" json:"file_name"`
-	FileURL    string    `bson:"fileUrl" json:"file_url"`
-	FileType   string    `bson:"fileType" json:"file_type"`
-	UploadedAt time.Time `bson:"uploadedAt" json:"uploaded_at"`
-}
+	type Attachment struct {
+		ID           uuid.UUID `bson:"_id" json:"id"`
+		FileName     string    `bson:"fileName" json:"file_name"`
+		FileURL      string    `bson:"fileUrl" json:"file_url"`
+		FileType     string    `bson:"fileType" json:"file_type"`
+		FileSize     int64     `bson:"fileSize" json:"file_size"`
+		UploadedAt   time.Time `bson:"uploadedAt" json:"uploaded_at"`
+	}
