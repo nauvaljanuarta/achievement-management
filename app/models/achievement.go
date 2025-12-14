f5919c90da20e44cdc1a9cfd1cf5e2d813246da5
ndajatoh01 <nauvalaryanto2005@gmail.com>
2025-12-14 14:00:59 +0700

    change minor string student id on mongo models (v 9.11.19)

--- app/models/achievement.go
+++ app/models/achievement.go
@@ -2,26 +2,26 @@ package models
 
 import (
 	"time"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-
 type Achievement struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	StudentID   uuid.UUID             `bson:"studentId" json:"student_id"`
-	AchievementType string         `bson:"achievementType" json:"achievement_type"`
-	Title       string             `bson:"title" json:"title"`
-	Description string             `bson:"description" json:"description"`
+	ID              primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	StudentID       string        `bson:"studentId" json:"student_id"`
+	AchievementType string             `bson:"achievementType" json:"achievement_type"`
+	Title           string             `bson:"title" json:"title"`
+	Description     string             `bson:"description" json:"description"`
 
-	Details     AchievementDetails `bson:"details" json:"details"`
+	Details AchievementDetails `bson:"details" json:"details"`
 
-	Attachments []Attachment       `bson:"attachments" json:"attachments"`
-	Tags        []string           `bson:"tags" json:"tags"`
-	Points      int                `bson:"points" json:"points"`
+	Attachments []Attachment `bson:"attachments" json:"attachments"`
+	Tags        []string     `bson:"tags" json:"tags"`
+	Points      int          `bson:"points" json:"points"`
 
-	CreatedAt   time.Time          `bson:"createdAt" json:"created_at"`
-	UpdatedAt   time.Time          `bson:"updatedAt" json:"updated_at"`
+	CreatedAt time.Time `bson:"createdAt" json:"created_at"`
+	UpdatedAt time.Time `bson:"updatedAt" json:"updated_at"`
 }
 
 type CreateAchievementRequest struct {
@@ -32,4 +32,5 @@ type CreateAchievementRequest struct {
 	Attachments     []Attachment       `json:"attachments"`
 	Tags            []string           `json:"tags"`
 	Points          int                `json:"points"`
+	StudentID       *uuid.UUID         `json:"student_id,omitempty"`
 }
